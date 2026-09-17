8b45789a6fc1f6142e269ce3f437303206bf536c
maydaygjw <maydaygjw@gmail.com>
2026-09-17 13:18:55 +0800

    chore(deploy): enable activity AI formatting in test

--- governance/SCRIPTS/start-yshop.sh
+++ governance/SCRIPTS/start-yshop.sh
@@ -46,16 +46,16 @@ ssh "${DEPLOY_USER}@${SERVER_HOST}" "
   if systemctl list-unit-files | grep -q yshop.service; then
     if [ \"${ENV_NAME}\" = \"test\" ]; then
       systemctl import-environment DASHSCOPE_API_KEY
-      systemctl set-environment ADAPAY_DEBUG=true AI_IMAGE_ENABLED=true LOGGING_LEVEL_CO_YIXIANG_YSHOP_MODULE_MP_PRIVATEPROMOTE_PRIVATEPROMOTEWEBHOOKCLIENT=DEBUG
+      systemctl set-environment ADAPAY_DEBUG=true AI_IMAGE_ENABLED=true AI_FORMAT_ENABLED=true LOGGING_LEVEL_CO_YIXIANG_YSHOP_MODULE_MP_PRIVATEPROMOTE_PRIVATEPROMOTEWEBHOOKCLIENT=DEBUG
     else
-      systemctl unset-environment ADAPAY_DEBUG AI_IMAGE_ENABLED DASHSCOPE_API_KEY || true
+      systemctl unset-environment ADAPAY_DEBUG AI_IMAGE_ENABLED AI_FORMAT_ENABLED DASHSCOPE_API_KEY || true
       systemctl set-environment SPRING_PROFILES_ACTIVE="${SPRING_PROFILES_ACTIVE:-}"
     fi
     systemctl start yshop.service
   else
     cd ${YSHOP_START_PATH}
     if [ \"${ENV_NAME}\" = \"test\" ]; then
-      ADAPAY_DEBUG=true AI_IMAGE_ENABLED=true nohup java -jar target/${YSHOP_JAR} --spring.profiles.active=dev --logging.level.co.yixiang.yshop.module.mp.privatepromote.PrivatePromoteWebhookClient=DEBUG > ${YSHOP_START_PATH}/app.log 2>&1 &
+      ADAPAY_DEBUG=true AI_IMAGE_ENABLED=true AI_FORMAT_ENABLED=true nohup java -jar target/${YSHOP_JAR} --spring.profiles.active=dev --logging.level.co.yixiang.yshop.module.mp.privatepromote.PrivatePromoteWebhookClient=DEBUG > ${YSHOP_START_PATH}/app.log 2>&1 &
     else
       nohup java -jar target/${YSHOP_JAR} --spring.profiles.active="${SPRING_PROFILES_ACTIVE:-}" > ${YSHOP_START_PATH}/app.log 2>&1 &
     fi
